1e79e68f888e40ecc7c49d0545196e333c698aa2
Gabor <gabor.gyorgy@and.digital>
2020-08-20 11:43:34 +0100

    Implemented PageNotFound component

---
+++ src/components/js/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './PageNotFound.css';
+
+const PageNotFound = () => {
+  return (
+    <div className="page-not-found-container">
+      <img src="https://memegenerator.net/img/instances/54555936/thou-shall-not-pass.jpg" alt="memez" />
+    </div>
+  );
+};
+
+export default PageNotFound;
