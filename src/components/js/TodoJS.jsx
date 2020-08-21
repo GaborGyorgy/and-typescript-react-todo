6009ca16340db9718b34468c442dbb94a2d74eca
Gabor <gabor.gyorgy@and.digital>
2020-08-21 10:29:11 +0100

    Added TodoJS

--- src/components/js/TodoJS.jsx
+++ src/components/js/TodoJS.jsx
@@ -1,19 +1,79 @@
 import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+
 import "../css/Todo.css";
 import AddTask from "./AddTask";
+import { getLocalStorageItem, setLocalStorageItem } from "./localStorageHelper";
+import TaskList from "./TaskList";
+import DeleteTaskModal from "./DeleteTaskModal";
 
-const TodoJS = () => {
+const LOCAL_STORAGE_KEY = "savedTasks";
+
+const TodoJS = ({ headerText, noTaskText }) => {
+  //javascript version of overloading - JS does not support it natively
+  if ((headerText && !noTaskText) || (!headerText && noTaskText))
+    throw new Error(
+      "You have to provide both headerText and noTaskText or neither."
+    );
+
   const [tasks, setTasks] = useState([]);
+  const [taskToDeleteIndex, setTaskToDeleteIndex] = useState(null);
+
+  useEffect(() => {
+    const savedTasks = getLocalStorageItem(LOCAL_STORAGE_KEY, []);
+    setTasks(savedTasks);
+  }, []);
+
   const addTaskHandler = (task) => {
-    console.log("task", task);
+    const tasksCopy = [...tasks];
+    tasksCopy.push({ label: task, isComplete: false });
+
+    setTasks(tasksCopy);
+    setLocalStorageItem(LOCAL_STORAGE_KEY, tasksCopy);
+  };
+
+  const deleteTaskHandler = (index) => {
+    setTaskToDeleteIndex(index);
+  };
+
+  const deleteConfirmedHandler = () => {
+    const tasksCopy = [...tasks];
+    tasksCopy.splice(taskToDeleteIndex, 1);
+    setTaskToDeleteIndex(null);
+    setTasks(tasksCopy);
+    setLocalStorageItem(LOCAL_STORAGE_KEY, tasksCopy);
+  };
+
+  const deleteCancelledHandler = () => {
+    setTaskToDeleteIndex(null);
   };
 
   return (
     <div className="container">
-      <h3 className="title">Todo JSX example</h3>
+      <h3 className="title">{headerText}</h3>
       <AddTask addTaskHandler={addTaskHandler} />
+      {tasks.length ? (
+        <TaskList tasks={tasks} deleteTaskHandler={deleteTaskHandler} />
+      ) : (
+        <div>{noTaskText}</div>
+      )}
+      <DeleteTaskModal
+        deleteCancelledHandler={deleteCancelledHandler}
+        deleteConfirmedHandler={deleteConfirmedHandler}
+        isOpen={taskToDeleteIndex !== null}
+      />
     </div>
   );
+};
+
+TodoJS.propTypes = {
+  headerText: PropTypes.string,
+  noTaskText: PropTypes.string,
+};
+
+TodoJS.defaultProps = {
+  headerText: "",
+  noTaskText: "",
 };
 
 export default TodoJS;
