d0e7c33831e42c6650f0281fb686388162789689
Gabor <gabor.gyorgy@and.digital>
2020-08-21 18:51:46 +0100

    Changed 404 page to TS

---
+++ src/components/ts/PageNotFound.tsx
@@ -0,0 +1,15 @@
+import React, { ReactElement } from "react";
+import "../css/PageNotFound.css";
+
+const PageNotFound = (): ReactElement => {
+  return (
+    <div className="page-not-found-container">
+      <img
+        src="https://memegenerator.net/img/instances/54555936/thou-shall-not-pass.jpg"
+        alt="memez"
+      />
+    </div>
+  );
+};
+
+export default PageNotFound;
