c5bcf7a627d284041fb9c806e0f5d174dd0d6a60
Gabor <gabor.gyorgy@and.digital>
2020-08-21 10:28:39 +0100

    Implemented DeleteTaskModal

---
+++ src/components/js/DeleteTaskModal.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Modal, Fade, Button } from "@material-ui/core";
+import "../css/DeleteTaskModal.css";
+
+const DeleteTaskModal = ({
+  deleteConfirmedHandler,
+  deleteCancelledHandler,
+  isOpen,
+}) => {
+  return (
+    <Modal open={isOpen} className="delete-task-modal">
+      <Fade in={isOpen} className="delete-task-modal-fade">
+        <div>
+          <h3 className="delete-task-modal-header">Deletion confirmation.</h3>
+          <p>Are you sure you want to delete the selected task?</p>
+          <div className="delete-task-modal-button-container">
+            <Button
+              variant="contained"
+              size="medium"
+              color="secondary"
+              onClick={deleteCancelledHandler}
+            >
+              No
+            </Button>
+            <Button
+              variant="contained"
+              size="medium"
+              color="primary"
+              onClick={deleteConfirmedHandler}
+            >
+              Yes
+            </Button>
+          </div>
+        </div>
+      </Fade>
+    </Modal>
+  );
+};
+
+export default DeleteTaskModal;
